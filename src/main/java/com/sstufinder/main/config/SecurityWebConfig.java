992dd432c6f63190012f779f5e4af9ddb9dc8f87
kyz9 <ikyz9.work@gmail.com>
2022-05-30 03:14:43 +0400

    feat: stable version on 30.05.22

--- src/main/java/com/sstufinder/main/config/SecurityWebConfig.java
+++ src/main/java/com/sstufinder/main/config/SecurityWebConfig.java
@@ -1,16 +1,16 @@
-//package com.sstufinder.main.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class SecurityWebConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:3000")
-//                .allowedMethods("*");
-//    }
-//}
+package com.sstufinder.main.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecurityWebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*");
+    }
+}
